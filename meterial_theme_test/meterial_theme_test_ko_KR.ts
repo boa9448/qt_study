7c531862678d1ba389b31f5a56456283275203aa
WDW <boa3465@gmail.com>
2022-05-26 01:55:09 +0900

    테마 적용 프로젝트 추가

---
+++ meterial_theme_test/meterial_theme_test_ko_KR.ts
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="21"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <location filename="mainwindow.ui" line="35"/>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>RadioButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
